19ab2e902e05b60c1ccafd3772391beebcf62298
Felixcodet <84507975+Felixcodet@users.noreply.github.com>
2022-01-25 07:09:10 +0100

    current

--- src/main.js
+++ src/main.js
@@ -1,27 +1,50 @@
 console.log('hello world')
-
-anime({
-    targets: '.logo',
-    scale: [
-      {value: .6, easing: 'linear', duration: 500},
-      {value: 1, easing: 'linear', duration: 1200}
-    ],
-    delay: anime.stagger(200, {grid: [68, 39], from: 'center'})
-  });
+const circles = document.querySelectorAll('.circle')
 
 // anime({
-//     targets: '.circle',
-//     translateX: 250
+//     targets: circles,
+//     scale: [
+//       {value: .6, easing: 'linear', duration: 500},
+//       {value: 1, easing: 'linear', duration: 1200}
+//     ],
+//     delay: anime.stagger(200, {grid: [68, 39], from: 'center'})
 //   });
 
+anime({
+    targets: circles,
+    translateX: 50
+  });
 
+  // anime({
+  //   targets: ".dropsDiv",
+  //   translateX: 250,
+  //   delay: 2000
+  // });
 
+  const movePath = {
+    curviness: 1,
+    autoRotate: false,
+    values: [
+      { x: 1500}
+    ]
+  };
 
+const tween =  new TimelineLite();
 
-// var elements = document.querySelectorAll('.logo');
+tween.add(
+  TweenLite.to(".dropsDiv",1,{
+   bezier: movePath,
+    ease: Power1.easeInOut
+  })
+)
 
-// anime({
-//   targets: elements,
-//   translateX: 1000,
-//   duration: 3000
-// });
+const controller = new ScrollMagic.Controller();
+
+const scene = new ScrollMagic.Scene({
+  triggerElement: '#introduction',
+  duration: 3000,
+})
+
+.setTween(tween)
+.addIndicators()
+.addTo(controller)
